27879bb924cc1743798f42c4a251e31575493fbd
Maksym Kondratiev <80330082+mkondratiev-wix@users.noreply.github.com>
2023-10-16 09:12:33 +0100

    add reactive vars state for favorites

---
+++ src/store/favoriteRepositories.ts
@@ -0,0 +1,52 @@
+import { ReactiveVar, makeVar } from "@apollo/client";
+import omit from "lodash.omit";
+import type { Repository, RepositoryOwner } from "../__generated__/graphql";
+
+export interface GithubRepository
+  extends Pick<
+    Repository,
+    "id" | "name" | "nameWithOwner" | "description" | "url" | "homepageUrl"
+  > {
+  owner: Pick<RepositoryOwner, "avatarUrl">;
+  rating: number;
+}
+
+type FavoriteReposState = Record<string, GithubRepository>;
+
+const getFavoriteRepos: ReactiveVar<FavoriteReposState> =
+  makeVar<FavoriteReposState>({});
+
+const isFavorite = (id: string) => {
+  const allRepos = getFavoriteRepos();
+  return id in allRepos;
+};
+
+const addToFavorites = (newRepo: GithubRepository) => {
+  const allRepos = getFavoriteRepos();
+  getFavoriteRepos({ ...allRepos, [newRepo.id]: newRepo });
+};
+
+const toggleFavorite = (repo: GithubRepository) => {
+  const allRepos = getFavoriteRepos();
+  if (isFavorite(repo.id)) {
+    const omited = omit(allRepos, [repo.id]);
+    return getFavoriteRepos(omited);
+  }
+
+  return getFavoriteRepos({ ...allRepos, [repo.id]: repo });
+};
+
+const setRating = (id: string, rating: number) => {
+  const allRepos = getFavoriteRepos();
+  if (isFavorite(id)) {
+    return getFavoriteRepos({ ...allRepos, [id]: { ...allRepos[id], rating } });
+  }
+};
+
+export {
+  addToFavorites,
+  getFavoriteRepos,
+  isFavorite,
+  toggleFavorite,
+  setRating,
+};
