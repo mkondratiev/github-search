7409f9ce1068a15af0a102a00315ed7087b44ffa
Maksym Kondratiev <80330082+mkondratiev-wix@users.noreply.github.com>
2023-10-16 12:09:45 +0100

    main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { ApolloProvider } from "@apollo/client";
+import CssBaseline from "@mui/material/CssBaseline";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { client } from "./cache/cache.ts";
+import { router } from "./components/pages/index.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CssBaseline />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </React.StrictMode>
+);
